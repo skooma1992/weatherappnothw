ead4451da8fd64f4c2e410a2be215c53683a6eaf
Jake <upsonjake@gmail.com>
2020-02-21 00:22:59 -0500

    commit

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ let long;
 let lat;
 let temperatureDescription = document.querySelector(".temperature-description");
 let temperatureDegree = document.querySelector(".temperature-degree");
-let locationTimezon = document.querySelector(".location-timezone")
+let locationTimezon = document.querySelector(".location-timezone");
+let temperatureSection = document.querySelector(".degree-secton");
+const temperatureSpan = document.querySelector(".degree-section span");
+
 if(navigator.geolocation){
 navigator.geolocation.getCurrentPosition(position => {
     long = position.coords.longitude;
@@ -18,17 +21,28 @@ navigator.geolocation.getCurrentPosition(position => {
     })
     .then(data => {
         console.log(data);
-        const {temperature, summary }= data.currently;
+        const {temperature, summary, icon }= data.currently;
         //set Dom elements from the api
         temperatureDegree.textContent = temperature;
         temperatureDescription.textContent = summary;
         locationTimezon.textContent = data.timezone;
+
+        setIcons(icon, document.querySelector(".icon"))
+        //change temperature to the bad kind
+        temperatureSection.addEventListener('click', () =>{
+            if(temperatureSpan.textContent === "F"){
+                temperatureSpan.textContent ==="C";
+            } else {temperatureSpan.textContent === "F"}
+        })
+        
     })
 });
 
 }
     function setIcons(icon, iconID){
-        const skycons = new skycons({color: "white"});
-        const currentIcon = 
+        const skycons = new Skycons({color: "white"});
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon])
     }
 });
